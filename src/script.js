1e862d1549e2af24c79ad0fbab344d5f6da1440f
BrunoOller <brunobrunelli4@gmail.com>
2025-02-16 20:46:14 -0300

    Atualizando a parte dos projetos.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-document.getElementsByClassName('.products')
+// Animação de scroll
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+
+elements.forEach((element) => myObserver.observe(element))
